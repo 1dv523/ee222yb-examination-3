aea5081b2f608b78c707e7f52070f2e2fb5d40dc
Bumpfel <34800245+Bumpfel@users.noreply.github.com>
2019-12-21 22:29:36 +0100

    git web hook in place
    gets issues from git on webhook update and client updates content

--- app.js
+++ app.js
@@ -16,29 +16,35 @@ const io = ws(server)
 
 app.use(express.static(path.join(__dirname, '/public')))
 
-// socket.emit skickar endast till samma klient som skickade
-//   kan vara bra om man inte vill skicka samma meddelande till just den klienten som till de andra
-// socket.broadcast skickar till alla anslutna klienter, förutom den klienten som skickade meddelandet
-// io.emit skickar till ALLA inkl. klienten som skickade.
-//   bör användas för att visa de flesta meddelanden så att klienten som skickar får verifierat att dennes meddelanden kommit fram
-
-// github
 const gitCredentials = require('./credentials/git-token.json')
 const client = github.client(gitCredentials.token)
-let issues
 const org = '1dv523'
 const repo = 'ee222yb-examination-3'
 
-client.get('/repos/' + org + '/' + repo + '/issues', {}, (req, status, body, headers) => { // TODO hämtar bara öppna issues verkar det som
-  // console.log(body)
-  issues = body
+const ghrepo = client.repo(org + '/' + repo)
+
+// create hook to github
+ghrepo.hook({
+  name: 'web',
+  active: true,
+  events: ['issues'], //, issue_comment
+  config: {
+    url: 'http://18c12bd2.ngrok.io/payload'
+  }
+}, () => console.log('created web hook'))
+
+// receive data from hook
+app.post('/payload', (req, res) => {
+  getIssues(io)
 })
 
-io.on('connection', socket => {
-  socket.emit('firstIssues', { repo: repo, issues: issues })
+// send issues to client on initial websocket connect
+io.on('connection', async socket => {
+  getIssues(socket)
+})
 
-  socket.on('', data => {
-    console.log('ws connection')
-    // io.emit('', { data: data.data })
+const getIssues = async socket => {
+  client.get('/repos/' + org + '/' + repo + '/issues', { state: 'all' }, (req, status, body, headers) => {
+    socket.emit('issues', { repo: repo, issues: body })
   })
-})
+}
