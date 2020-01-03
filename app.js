ea416e35fcf6ce3b34da60eeed415a53fd9ac87d
Bumpfel <34800245+Bumpfel@users.noreply.github.com>
2020-01-03 15:23:08 +0100

    hook secret and signature validation

--- app.js
+++ app.js
@@ -1,68 +1,90 @@
 'use strict'
 
-// if (process.env.NOVE_ENV !== 'production') {
 require('dotenv').config()
-// }
 
 const github = require('octonode')
-// const gitWebHook = require('express-github-webhook')
-// const webhookHandler = require('express-github-webhook')({ path: '/webhook', secret: 'process.env.GIT_TOKEN' })
 const express = require('express')
 const ws = require('socket.io')
 const path = require('path')
 const crypto = require('crypto')
+const bodyParser = require('body-parser')
 
 const app = express()
-
-const server = app.listen(process.env.PORT, () => {
-  console.log('Server listening on port ' + process.env.PORT)
-})
-
-const io = ws(server)
-
-app.use(express.static(path.join(__dirname, '/public')))
+const port = process.env.PORT || 8000
 
 const org = '1dv523'
 const repo = 'ee222yb-examination-3'
-
 const client = github.client(process.env.GIT_TOKEN)
 const ghrepo = client.repo(org + '/' + repo)
 
-// create hook to github
-ghrepo.hook({
-  name: 'web',
-  active: true,
-  events: ['issues', 'issue_comment'],
-  config: {
-    secret: process.env.GIT_TOKEN,
-    url: 'http://87ced3b5.ngrok.io/issuehook'
-  }
-}, () => console.log('Registered web hook'))
-
-// receive data from hook
-app.post('/issuehook', req => {
-  console.log()
-  console.log('received hook response')
-  // TODO kontrollera x-hub-signature
-
-  const secret = req.headers['x-hub-signature']
-  console.log(secret)
-  const hex = crypto.createHmac('sha1', process.env.GIT_TOKEN)
-  console.log(hex)
-
-  getAndSendIssues(io)
+const server = app.listen(port, () => {
+  console.log('Server listening on port ' + port)
 })
 
+const io = ws(server)
 // send issues to client on initial websocket connect
 io.on('connection', socket => {
   getAndSendIssues(socket)
 })
 
+app.use(express.static(path.join(__dirname, '/public')))
+
+/* ***************************************************** */
+/* TODO eventuellt extrahera denna del till extern modul */
+const validateHookOrigin = (req, res, buf, encoding) => {
+  // console.log('validating web hook...')
+
+  const hmac = crypto.createHmac('sha1', process.env.GIT_SECRET)
+  hmac.update(buf, 'utf-8')
+  const expectedSignature = 'sha1=' + hmac.digest('hex')
+
+  if (req.headers['x-hub-signature'] !== expectedSignature) {
+    throw new Error('Received invalid signature')
+  }
+  // } else {
+  //   console.log('Valid signature!')
+  // }
+}
+
+// use body parser middle ware to validate hook origin
+app.use(bodyParser.urlencoded({ verify: validateHookOrigin, extended: true }))
+// app.use(bodyParser.json({ verify: validateHookOrigin }))
+
+// abort hook handling on error (ORDER IMPORTANT)
+app.use((err, req, res, next) => {
+  if (!err) {
+    next()
+  } else {
+    console.log(err)
+  }
+})
+
+// handle received data from hook
+app.post('/issuehook', (req, res) => {
+  console.log('received response from web hook')
+
+  getAndSendIssues(io)
+})
+
+/* ****************** */
+/* ****************** */
+
+// create/overwrite webhook on github
+ghrepo.hook({
+  name: 'web',
+  active: true,
+  events: ['issues', 'issue_comment'],
+  config: {
+    secret: process.env.GIT_SECRET,
+    url: 'http://5a3079b9.ngrok.io/issuehook'
+  }
+}, () => console.log('Registered web hook'))
+
 const getAndSendIssues = socket => {
   client.get('/repos/' + org + '/' + repo + '/issues', { state: 'all' }, (err, status, body, headers) => {
     if (err) {
-      // TODO server error
-      // console.error(err)
+      // TODO error
+      console.error('ERROR', err.statusCode, err.message)
     }
     socket.emit('issues', { repo: repo, issues: body })
   })
@@ -71,7 +93,7 @@ const getAndSendIssues = socket => {
 const getComments = (socket, issueNr) => {
   client.get('/repos/' + org + '/' + repo + '/issues/' + issueNr + '/comments', {}, (err, status, body, headers) => {
     if (err) {
-      // TODO server error
+      // TODO error
       console.error(err)
     }
     console.log(body)
